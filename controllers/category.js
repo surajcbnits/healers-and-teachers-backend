ef86791948c0bfdc1a6ea13a169fc123a327f82a
surajcbnits <suraj.biswas@cbnits.com>
2023-01-04 11:49:12 +0530

    category api added

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,10 @@
-const db = require('../models');
+const db = require("../models");
+const { onlyUnique } = require("../utils");
 const Category = db.category;
+const WellnessKeywords = db.wellnessKeywords;
+const Member = db.member;
+const WellnessMapping = db.wellnessMapping;
+const MemberEvents = db.memberEvents;
 
 exports.createCategoryController = async (req, res) => {
   // destructor the fields
@@ -8,12 +13,11 @@ exports.createCategoryController = async (req, res) => {
   // check if the fields are empty
   if (!name || !description) {
     return res.status(400).json({
-      message: 'Please fill name,description the fields',
+      message: "Please fill name,description the fields",
     });
   }
 
   try {
-       
     // creating the Category
     const data = await Category.create({
       name,
@@ -21,15 +25,15 @@ exports.createCategoryController = async (req, res) => {
       description,
     });
 
-    console.log('CategoryKeywords created data : ', data);
+    console.log("CategoryKeywords created data : ", data);
 
-    res.status(200).json(data );
+    res.status(200).json(data);
   } catch (error) {
-    console.log('error : ', error);
+    console.log("error : ", error);
     res.status(500).json({
       message:
         error.message ||
-        'Some error occurred while Fetching the WellnessKeywords.',
+        "Some error occurred while Fetching the WellnessKeywords.",
     });
   }
 };
@@ -38,12 +42,12 @@ exports.getAllCategoryController = async (req, res) => {
   try {
     const data = await Category.findAll({
       attributes: [
-        'id',
-        'name',
-        'image',
-        'description',
-        'createdAt',
-        'updatedAt',
+        "id",
+        "name",
+        "image",
+        "description",
+        "createdAt",
+        "updatedAt",
       ],
     });
 
@@ -51,7 +55,137 @@ exports.getAllCategoryController = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: err?.message || 'Some error occurred while creating the Member.',
+      message:
+        err?.message || "Some error occurred while fetching the Categories.",
+    });
+  }
+};
+
+exports.getCategoryDetailsByIdController = async (req, res) => {
+  const { id } = req.query;
+
+  if (!id) {
+    res.status(500).json("Id is mandatory");
+  }
+
+  try {
+    const data = await Category.findOne({
+      where: { id: id },
+      attributes: [
+        "id",
+        "name",
+        "image",
+        "description",
+        "createdAt",
+        "updatedAt",
+      ],
+    });
+
+    if (data?.dataValues?.id) {
+      // fetching the wellnessKeywords that is attached to the category
+      const wellnessKeywordData = await WellnessKeywords.findAll({
+        where: { CategoryId: data.dataValues.id },
+      });
+      console.log("wellnessKeywordData", wellnessKeywordData);
+
+      // clear the wellnessKeywords data with the name and id
+      const wellnesskeywords = wellnessKeywordData.length
+        ? wellnessKeywordData.map((i) => {
+            return { id: i.dataValues.id, name: i.dataValues.name };
+          })
+        : [];
+
+      // getting all the members that is being mapped to the current wellnesskeywords
+      const wellnessMappingDataForMember = await Promise.all(
+        wellnesskeywords.map(async (i) => {
+          console.log("i.id : ", i.id);
+          const id = await WellnessMapping.findAll({
+            where: { WellnessKeywordId: i.id, type: "member" },
+          });
+
+          return id;
+        })
+      );
+      // clearing the data and getting the memberId array
+      const memberIds = wellnessMappingDataForMember
+        .map((i) => i[0]?.dataValues?.genarelid)
+        .filter((i) => i)
+        .filter(onlyUnique);
+
+      //  fetching member details from member id array
+      const memberDetails = memberIds?.length
+        ? await Promise.all(
+            memberIds?.map(async (i) => {
+              const data = await Member.findOne({
+                attributes: [
+                  "id",
+                  "firstName",
+                  "lastName",
+                  "username",
+                  "email",
+                  "city",
+                  "state",
+                  "country",
+                  "phoneno",
+                  "website",
+                  "aboutme",
+                  "title",
+                  "qualification",
+                  "facebook",
+                  "instagram",
+                  "twitter",
+                  "image",
+                  "physicaladdress",
+                  "ip",
+                  "virtualsessions",
+                ],
+                where: { id: i, accountstatus: "active" },
+              });
+              return data;
+            })
+          )
+        : [];
+
+      // getting all the events that is being mapped to the current wellnesskeywords
+      const wellnessMappingDataForEvents = await Promise.all(
+        wellnesskeywords.map(async (i) => {
+          console.log("i.id : ", i.id);
+          const id = await WellnessMapping.findAll({
+            where: { WellnessKeywordId: i.id, type: "event" },
+          });
+          return id;
+        })
+      );
+      // clearing the data and getting the eventId array
+      const eventIds = wellnessMappingDataForEvents
+        ?.map((i) => i[0]?.dataValues?.genarelid)
+        .filter((i) => i)
+        .filter(onlyUnique);
+
+      //  fetching event details from event id array
+      const eventDetails = eventIds?.length
+        ? await Promise.all(
+            eventIds?.map(async (i) => {
+              const data = await MemberEvents.findOne({
+                where: { id: i, eventstatus: "active" },
+              });
+              return data;
+            })
+          )
+        : [];
+
+      data.dataValues.wellnesskeywords = wellnesskeywords;
+      data.dataValues.memberDetails = memberDetails;
+      data.dataValues.eventDetails = eventDetails;
+    }
+
+    console.log("data : ", data);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: err?.message || "Some error occurred while creating the Member.",
     });
   }
 };
--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,7 @@ var express = require('express');
 const {
   createCategoryController,
   getAllCategoryController,
+  getCategoryDetailsByIdController,
 } = require('../controllers/category');
 const { fileUpload } = require('../middleware');
 var router = express.Router();
@@ -12,5 +13,6 @@ router.post(
   createCategoryController
 );
 router.get('/getAllCategoryList', getAllCategoryController);
+router.get('/getCategoryDetailsById', getCategoryDetailsByIdController);
 
 module.exports = router;
